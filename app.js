27e23b4cbd0ce71f6c4bca3a2236099bd7f6226c
Galiant <antonijodev@gmail.com>
2019-01-15 17:28:31 +0000

    Display the weather

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 // Init weather object
 const weather = new Weather('Dublin', 'IE');
 
+// Init UI
+const ui = new UI();
+
 // Get weather on DOM load
 document.addEventListener('DOMContentLoaded', getWeather);
 
@@ -9,7 +12,7 @@ document.addEventListener('DOMContentLoaded', getWeather);
 function getWeather() {
   weather.getWeather()
     .then(results => {
-      console.log(results);
+      ui.paint(results);
     })
     .catch(err => console.log(err));
 }
